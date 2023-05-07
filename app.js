257062b2efddad0831adae013aad4ec97868c5aa
MiguelAlejandroRodriguezCruz <129399935+MiguelAlejandroRodriguezCruz@users.noreply.github.com>
2023-05-07 17:18:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = process.env.port || 80;
+const port = process.env.port || 3000;
 
 const app = express();
 
